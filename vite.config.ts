78b06ac96d03712b550d3f8f4af41876adff428d
Muh-Adib <muhammad.17@students.amikom.ac.id>
2025-12-11 12:44:14 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -68,7 +68,7 @@ export default defineConfig({
     },
     resolve: {
         alias: {
-            'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
+            // 'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
         },
     },
     define: {
