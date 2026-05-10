3679681b0fca4adacd1963634a10d7948ef8751e
Muh-Adib <muhammad.17@students.amikom.ac.id>
2026-05-10 11:16:06 +0700

    modified:   resources/views/app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,15 @@
     <meta name="msvalidate.01" content="B9BB8EBFA882C4F41605AE528EA70F7D" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <!-- Clarity tracking code for https://homsjogja.com -->
+    <script>
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "woquourldg");
+    </script>
+
     {{-- Inline script to detect system dark mode preference and apply it immediately --}}
     <script>
         (function () {
